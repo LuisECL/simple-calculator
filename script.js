a369ec00388bfe7ad326fac17ed6555d79f3bcdc
Luis <lecarcamol@gmail.com>
2022-05-15 23:42:26 -0300

    Clear button added

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 window.onload = function() {
+  // DOM elements
   let screen = document.getElementById('screen');
   let numbersHTML = document.getElementsByClassName('number')
   let numbers = Array.from(numbersHTML)
   let btn_plus = document.getElementById('btn-plus')
+  let btn_clear = document.getElementById('clear')
   let screenDisplay = ""
   let calculation = 0
 
-  // screen.innerHTML = 1+5
-  // console.log(number7.value)
-
-  function updateScreen(s) {
-    screen.innerText = screen.innerText + s
-  }
-
+  // Event Listeners
   numbers.forEach(e => {
     e.addEventListener('click', () => {
       updateScreen(e.value)
     })
   });
 
-
   btn_plus.addEventListener('click', () => {
     updateScreen(btn_plus.value);
   })
 
+  btn_clear.addEventListener('click', () => {
+    clearScreen()
+  })
 
 
+  // Functions
+  function updateScreen(s) {
+    screen.innerText = screen.innerText + s
+  }
 
-
+  function clearScreen() {
+    screen.innerText = ""
+  }
 
 
 }
