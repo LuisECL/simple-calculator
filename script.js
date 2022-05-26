4286da4cc3660c8cd45704056a2b68a61c9fc217
Luis <lecarcamol@gmail.com>
2022-05-25 22:38:32 -0300

    Operators updating screen

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ window.onload = function() {
   // DOM elements
   let screen = document.getElementById('screen');
   let numbersHTML = document.getElementsByClassName('number')
-  let numbers = Array.from(numbersHTML)
-  let btn_plus = document.getElementById('btn-plus')
-  let btn_clear = document.getElementById('clear')
+  let numbers = Array.from(numbersHTML);
+  let btn_plus = document.getElementById('btn-plus');
+  let btn_minus = document.getElementById('btn-minus');
+  let btn_multiply = document.getElementById('btn-multiply');
+  let btn_divide = document.getElementById('btn-divide');
+  let btn_clear = document.getElementById('clear');
+
+  let operators = [btn_plus, btn_minus, btn_multiply, btn_divide]
   let screenDisplay = ""
   let calculation = 0
 
@@ -15,8 +20,10 @@ window.onload = function() {
     })
   });
 
-  btn_plus.addEventListener('click', () => {
-    updateScreen(btn_plus.value);
+  operators.forEach(e => {
+    e.addEventListener('click', () => {
+      updateScreen(e.value);
+    });
   })
 
   btn_clear.addEventListener('click', () => {
